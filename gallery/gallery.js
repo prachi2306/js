19025883ce17fcfe975b664cc4dcc4dfa0606608
prachi2306 <prachipawar2203@gmail.com>
2024-06-05 12:40:30 +0530

    Introduction to Javascript

---
+++ gallery/gallery.js
@@ -0,0 +1,22 @@
+var artworks = [
+    {
+      description: "A lake in Norway in autumn.",
+      address: "https://picsum.photos/id/123/400"
+    },
+    {
+      description: "A Red flower",
+      address: "https://picsum.photos/id/234/400"
+    },
+    {
+      description: "A House in the US",
+      address: "https://picsum.photos/id/809/400"
+    },
+    {
+      description: "Happiness and peace",
+      address: "https://picsum.photos/id/635/400"
+    },
+  ]
+  
+for (var i = 0; i < artworks.length; i++) {
+    console.log(artworks[i].description);
+  }
